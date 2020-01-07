d9afe89b74bda81b560b39dff111f936cd774458
bennyp85 <benjamintp.1985@gmail.com>
2020-01-07 23:11:11 +1100

    Test Git

---
+++ Assignment 2/Programming Project 2 (template)/DataStructures_Algorithms/Project02/HeapSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Algorithms.Project02
+{
+    public class HeapSort : ISorter
+    {
+        public HeapSort()
+        {
+        }
+
+        public void Sort<T>(T[] sequence, IComparer<T> comparer) where T : IComparable<T>
+        {
+            throw new NotImplementedException();
+        }
+
+        private class DefaultComparer<D, K> : IComparer<IHeapifyable<D, K>>
+        {
+            public int Compare(IHeapifyable<D, K> x, IHeapifyable<D, K> y)
+            {
+                return Comparer<K>.Default.Compare(x.Key, y.Key);
+            }
+        }
+    }
+}
